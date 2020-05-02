6e27838f1741808a558d8c1a2cc0fb26ec8ee3d8
rifandani <ipandani2505@gmail.com>
2020-05-02 17:29:28 +0800

    tambah LICENSE and Procfile, edit port di app.js

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ app.set('views', 'views');
 // using router file
 app.use('/', router);
 
-app.listen(3000, () => {
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 8000;
+}
+app.listen(port, () => {
   console.log('Server berjalan di port 3000');
 });
